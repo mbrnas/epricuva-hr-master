7bc5c5818747dba90af1fc74d97dc8ef96492142
Matija Brnas <mb8828@rit.edu>
2024-02-20 22:11:24 +0100

    initial commit

---
+++ epricuva-hr-backend/src/main/java/com/onecode/epricuvahrbackend/EpricuvaHrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.onecode.epricuvahrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpricuvaHrBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpricuvaHrBackendApplication.class, args);
+    }
+
+}
---
+++ epricuva-hr-backend/src/test/java/com/onecode/epricuvahrbackend/EpricuvaHrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onecode.epricuvahrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpricuvaHrBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
